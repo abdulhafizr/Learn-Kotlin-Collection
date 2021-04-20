3f70a07fc2a0b65d61b594ce72b89f68ba7dd4b0
Abdul Hafiz Ramadan <62394202+abdulhafizr@users.noreply.github.com>
2021-04-20 22:53:19 +0700

    | Random Ordering Collection

---
+++ src/app/RandomOrdering.kt
@@ -0,0 +1,10 @@
+package app
+
+fun main() {
+    val numbers: List<Int> = (1..10).toList()
+    
+    println(numbers.shuffled())
+    println(numbers.shuffled())
+    println(numbers.shuffled())
+    println(numbers)
+}
